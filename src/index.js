d48b80df3d5e0bdfb24ec3a62a8e930aaa4a3d63
roshkovskypetr <roshkovskypetr@gmail.com>
2019-09-23 01:20:47 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  const firstArray = first.split('').reverse();
+  const secondArray = second.split('').reverse();
+
+  let resultArray = [];
+
+  firstArray.map((el, index) => {
+    secondArray.map((el2, index2) => {
+      const m = el * el2;
+      resultArray[index + index2] = (resultArray[index + index2]) ? resultArray[index + index2] + m : m;
+    })
+  })
+
+  for (let i = 0; i < resultArray.length; i++) {
+    let dozens = Math.floor(resultArray[i] / 10);
+    let number = resultArray[i] % 10;
+    resultArray[i] = number;
+
+    if (resultArray[i + 1])
+      resultArray[i + 1] += dozens;
+    else if (dozens != 0)
+      resultArray[i + 1] = dozens;
+  }
+
+  return resultArray.reverse().join('')
 }
